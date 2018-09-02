e6c091f4ff92a42b6987085c19a93bab382f5fb3
Chisom Esele <esele001@umn.edu>
2018-09-02 02:23:36 -0500

    Array

--- cpll.cpp
+++ cpll.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 int main()
 {
-    // initialization expression
-    int i = 1;
+    double sales[10] = {12.50, 8.35, 19.60, 25.00, 14.00, 39.43, 35.90, 98.23, 66.65, 35.64};
+    int index;
+    double largestSale, sum, average;
 
-    // test expression
-    while (i < 6)
+    int maxIndex = 0;
+    for (index = 1; index < 10; index++)
     {
-        cout << "Hello World\n";
-
-        // update expression
-        i++;
+        if (sales[maxIndex] < sales[index])
+            maxIndex = index;
     }
+    largestSale = sales[maxIndex];
+
+    cout << "Largest sale  " << largestSale << endl;
+    cout << "maxIndex  " << maxIndex << endl;
 
     return 0;
 }
